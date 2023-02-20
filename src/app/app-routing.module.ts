feba3a1ec89541540ea7dfd1d5c98cdacccfef15
Rafal Fuz <rafalfuz@gmail.com>
2023-02-20 20:45:40 +0100

    made autologin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,6 +24,9 @@ const routes: Routes = [
     component: LoginFormComponent,
   },
   {
+    path: 'bucket-list',
+  },
+  {
     path: 'admin',
     component: AdminViewComponent,
     canActivate: [adminAuthGuard],
